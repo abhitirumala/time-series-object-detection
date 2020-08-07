78c37acbbeaffc283db8dc168565a1f32de2cd93
Tirumala, Abhiram Venkata <abhiram.tirumala@gatech.edu>
2020-08-07 12:31:33 -0400

    increase inference speeds

--- run_model.py
+++ run_model.py
@@ -1,12 +1,10 @@
 import time
 
-import keras
-import keras.backend as K
+import tensorflow.keras as keras
+import tensorflow as tf
 import numpy as np
 
 from utils import *
-
-keras.backend.set_learning_phase(0)
 
 
 def main():
@@ -35,14 +33,15 @@ def main():
         out.write(img)
 
     print('Processed Seed Frames.')
-    box_model = keras.models.load_model('./models/box_model_08_03_20.h5')
+    box_model = keras.models.load_model('./models/box_model_08_07_20.h5')
     box_model.trainable = False
-    # box_model.summary()
-    img_model = keras.models.load_model('./models/img_model_08_03_20.hdf5')
+    box_model.summary()
+    img_model = keras.models.load_model('./models/img_model_07_29_20.hdf5')
     img_model.trainable = False
-    # img_model.summary()
+    img_model.summary()
     print('Loaded models.\nApplying model to frames.')
 
+    total_fps = 0
     count = 0
 
     for frame in test_data:
@@ -53,21 +52,19 @@ def main():
         img = frame[0]
 
         input_boxes = np.expand_dims(
-            np.array(box_time_series[-frame_history:]), axis=0).astype(np.float32)
+            np.asarray(box_time_series[-frame_history:]), axis=0).astype(np.float32)
 
-        box_prediction = box_model.predict(
-            input_boxes, batch_size=1, verbose=0)
+        box_prediction = box_model.predict_on_batch(input_boxes)
         box_prediction = np.round(box_prediction)[0].astype(int)
 
         checkpoints.append(time.time() - last_time)
         last_time = time.time()
 
-        box_list = get_boxes(box_prediction, margin=(10, 10), num=(6, 6))
+        box_list = get_boxes(box_prediction, margin=(8, 8), num=(4, 5))
         cropped_imgs = np.asarray(
             [crop_and_resize(img, box) for box in box_list])
 
-        img_predictions = img_model.predict(
-            cropped_imgs, batch_size=50, verbose=0)
+        img_predictions = img_model.predict_on_batch(cropped_imgs)
         img_predictions = [map_to_class(pred) for pred in img_predictions]
 
         checkpoints.append(time.time() - last_time)
@@ -95,14 +92,14 @@ def main():
         out.write(img)
 
         count += 1
+        total_fps += (1.0 / (time.time() - start_time))
         print(
-            f'\rStatus: {count} out of {len(test_data)}, FPS: {(1.0 / (time.time() - start_time)):.4f}; {np.round(checkpoints, 3)}',
+            f'\rStatus: {count} out of {len(test_data)}, FPS: {(1.0 / (time.time() - start_time)):6.4f}; {np.round(checkpoints, 3)}',
             end='')
 
-        K.clear_session()
-
+    print(f'\nAverage FPS: {(total_fps/count):6.4f}')
     out.release()
-    print('\nVideo Saved.')
+    print('Video Saved.')
 
 
 if __name__ == '__main__':
